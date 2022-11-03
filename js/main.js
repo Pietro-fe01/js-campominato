1a6833934d8e774062ba0925446d5c4f0dd77079
Pietro-fe01 <pietrofecarotta2001@gmail.com>
2022-11-03 11:25:31 +0100

    Il messaggio iniziale per la scelta della difficoltà viene sempre visualizzato

--- js/main.js
+++ js/main.js
@@ -3,17 +3,6 @@
 /*---------------
     FUNCTIONS
 ---------------*/
-
-/* Bonus: Aggiungere una select accanto al bottone di generazione, che fornisca una 
-scelta tra tre diversi livelli di difficoltà:
-- con difficoltà 1 (insane) => 100 caselle, con un numero compreso tra 1 e 100, divise
- in 10 caselle per 10 righe;
-
-- con difficoltà 2 (hard) => 81 caselle, con un numero compreso tra 1 e 81, divise
- in 9 caselle per 9 righe;
-
-- con difficoltà 3 (normal) => 49 caselle, con un numero compreso tra 1 e 49, divise
- in 7 caselle per 7 righe; */
 function checkSelectValue(select){
     if (select.selectedIndex === 1){
         return 49;
@@ -38,8 +27,8 @@ Se non si sceglie la difficoltà, il gioco non parte.*/
 
 playGameButton.addEventListener("click", function(){
     if(selectBody.value === "default"){
-        infoGameButton.classList.remove("d-none");
     } else {
+        infoGameButton.classList.add("d-none");
         cellContainer.innerHTML = "";
         for(let i=1; i<= checkSelectValue(selectBody); i++){
             const cellSquare = document.createElement("div");
@@ -53,7 +42,8 @@ playGameButton.addEventListener("click", function(){
             cellSquare.innerHTML = i;
     
             /*Quando l'utente clicca su ogni cella, la cella cliccata si colora di 
-            azzurro ed emetto un messaggio in console con il numero della cella cliccata.*/
+            azzurro ed emetto un messaggio in console con il numero della cella 
+            cliccata.*/
             cellSquare.addEventListener("click", function(){
                 this.classList.add("blue-bg-color");
                 console.log(this.innerHTML);
